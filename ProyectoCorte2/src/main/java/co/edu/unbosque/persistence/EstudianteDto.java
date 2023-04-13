297b7e945f126e74b7f6c3ef475462c3f48c9ccd
Mystical Tree <jsebastianllamprea@gmail.com>
2023-04-13 18:48:48 -0500

    Subida Main
    Se hace la subida a la rama principal con dto creado, index con solo titulo y a partir de aquí se va a ir agregando la funcionalidad

---
+++ ProyectoCorte2/src/main/java/co/edu/unbosque/persistence/EstudianteDto.java
@@ -0,0 +1,74 @@
+package co.edu.unbosque.persistence;
+
+public class EstudianteDto {
+	private String nombre, colegio, fecha, carrera;
+	private int estrato, cedula;
+	private boolean homologado;
+
+	public EstudianteDto(String nombre, String fecha, String colegio, String carrera, int estrato, int cedula, boolean homologado) {
+		this.nombre = nombre;
+		this.colegio = colegio;
+		this.fecha = fecha;
+		this.estrato = estrato;
+		this.homologado = homologado;
+		this.cedula = cedula;
+		this.carrera = carrera;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getColegio() {
+		return colegio;
+	}
+
+	public void setColegio(String colegio) {
+		this.colegio = colegio;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public int getEstrato() {
+		return estrato;
+	}
+
+	public void setEstrato(int estrato) {
+		this.estrato = estrato;
+	}
+
+	public boolean isHomologado() {
+		return homologado;
+	}
+
+	public void setHomologado(boolean homologado) {
+		this.homologado = homologado;
+	}
+
+	public int getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(int cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getCarrera() {
+		return carrera;
+	}
+
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+	
+}
---
+++ ProyectoCorte2/src/main/java/co/edu/unbosque/services/Controller.java
@@ -0,0 +1,5 @@
+package co.edu.unbosque.services;
+
+public class Controller {
+
+}
